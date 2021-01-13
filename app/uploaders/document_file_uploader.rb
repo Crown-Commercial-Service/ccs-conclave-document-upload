6895013585b7b0b8753ade6a07bb17b1dbcce8fc
Iulia R Ionita <iulia@ionita.me>
2021-01-13 17:27:14 +0000

    Removing test files after running specs
    Added rails_helper changes

--- app/uploaders/document_file_uploader.rb
+++ app/uploaders/document_file_uploader.rb
@@ -13,7 +13,11 @@ class DocumentFileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    if Rails.env.test?
+      "uploads/test/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    else
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
