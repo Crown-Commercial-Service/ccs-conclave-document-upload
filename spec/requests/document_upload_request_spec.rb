0e06433b37ea4233d60cb13c155a87495efb81c3
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2021-02-15 10:37:47 +0000

    added support for doc and xls, changed test for unsupported file type to pptx

--- spec/requests/document_upload_request_spec.rb
+++ spec/requests/document_upload_request_spec.rb
@@ -109,9 +109,65 @@ RSpec.describe 'DocumentUploads', type: :request do
         end
       end
 
+      context 'when file is xls' do
+        let(:mime_type) { 'application/vnd.ms-excel' }
+        let(:file_name) { 'test_xls.xls' }
+
+        it 'creates a Document' do
+          expect do
+            post '/document-upload', params: valid_attributes, headers: headers
+          end.to change(Document, :count).by(1)
+        end
+
+        it 'creates an UncheckedDocument' do
+          expect do
+            post '/document-upload', params: valid_attributes, headers: headers
+          end.to change(UncheckedDocument, :count).by(1)
+        end
+
+        it 'does the PUT request' do
+          post '/document-upload', params: valid_attributes, headers: headers
+          expect(HTTParty).to have_received(:put).with(ENV['CHECK_ENDPOINT_URL'], body:
+            { unchecked_document_id: UncheckedDocument.last.id }, headers: { 'Authorization' => ENV['AUTH_TOKEN'] })
+        end
+
+        it 'returns status code 201' do
+          post '/document-upload', params: valid_attributes, headers: headers
+          expect(response).to have_http_status(201)
+        end
+      end
+
       context 'when file is docx' do
         let(:mime_type) { 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' }
         let(:file_name) { 'test_docx.docx' }
+
+        it 'creates a Document' do
+          expect do
+            post '/document-upload', params: valid_attributes, headers: headers
+          end.to change(Document, :count).by(1)
+        end
+
+        it 'creates an UncheckedDocument' do
+          expect do
+            post '/document-upload', params: valid_attributes, headers: headers
+          end.to change(UncheckedDocument, :count).by(1)
+        end
+
+        it 'does the PUT request' do
+          post '/document-upload', params: valid_attributes, headers: headers
+          expect(HTTParty).to have_received(:put).with(ENV['CHECK_ENDPOINT_URL'], body:
+            { unchecked_document_id: UncheckedDocument.last.id }, headers: { 'Authorization' => ENV['AUTH_TOKEN'] })
+        end
+
+        it 'returns status code 201' do
+          post '/document-upload', params: valid_attributes, headers: headers
+          expect(response).to have_http_status(201)
+        end
+      end
+
+      context 'when file is doc' do
+        let(:mime_type) { 'application/msword' }
+        let(:file_name) { 'test_doc.doc' }
 
         it 'creates a Document' do
           expect do
@@ -533,9 +589,9 @@ RSpec.describe 'DocumentUploads', type: :request do
     end
 
     context 'when file unsupported type' do
-      let(:xls_file) { fixture_file_upload('test_xls.xls', 'application/vnd.ms-excel') }
+      let(:pptx_file) { fixture_file_upload('test_pptx.pptx', 'application/vnd.openxmlformats-officedocument.presentationml.presentation') }
       let(:invalid_attributes) do
-        { document_file_path: '', document_file: xls_file, type_validation: ['xls'], size_validation: 1000000 }
+        { document_file_path: '', document_file: pptx_file, type_validation: ['pptx'], size_validation: 1000000 }
       end
 
       it 'does not create a Document' do
@@ -565,14 +621,14 @@ RSpec.describe 'DocumentUploads', type: :request do
     end
 
     context 'when file path unsupported type' do
-      let(:xls_file) { fixture_file_upload('test_xls.xls', 'application/vnd.ms-excel') }
-      let(:file_path) { 'https://www.example.com/test_xls.xls' }
+      let(:pptx_file) { fixture_file_upload('test_pptx.pptx', 'application/vnd.openxmlformats-officedocument.presentationml.presentation') }
+      let(:file_path) { 'https://www.example.com/test_pptx.pptx' }
       let(:invalid_attributes) do
-        { document_file_path: file_path, type_validation: ['xls'], size_validation: 1000000 }
+        { document_file_path: file_path, type_validation: ['pptx'], size_validation: 1000000 }
       end
 
       before do
-        stub_request(:get, 'https://www.example.com/test_xls.xls')
+        stub_request(:get, 'https://www.example.com/test_pptx.pptx')
           .with(
             headers: {
               'Accept' => '*/*',
@@ -580,7 +636,7 @@ RSpec.describe 'DocumentUploads', type: :request do
               'User-Agent' => 'CarrierWave/2.1.0'
             }
           )
-          .to_return(status: 200, body: File.open(xls_file), headers: {})
+          .to_return(status: 200, body: File.open(pptx_file), headers: {})
       end
 
       it 'does not create a Document' do
