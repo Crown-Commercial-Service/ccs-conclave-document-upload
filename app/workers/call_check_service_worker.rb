aeeec38d66aabdc2ba249080b9b5f70031f1bff0
Iulia R Ionita <iulia@ionita.me>
2021-04-27 17:08:50 +0100

    Changed POST path to /documents and PUT path to /documents/:document_id
    Fixed tests & rubocop

--- app/workers/call_check_service_worker.rb
+++ app/workers/call_check_service_worker.rb
@@ -5,7 +5,13 @@ class CallCheckServiceWorker
   def perform(unchecked_document_id)
     return unless ENV['CHECK_ENDPOINT_URL']
 
-    HTTParty.put(ENV['CHECK_ENDPOINT_URL'], body:
+    HTTParty.put(ENV['CHECK_ENDPOINT_URL'] + "/#{document_id(unchecked_document_id)}", body:
       { unchecked_document_id: unchecked_document_id }, headers: { 'x-api-key' => ENV['AUTH_TOKEN'] })
+  end
+
+  private
+
+  def document_id(unchecked_document_id)
+    UncheckedDocument.find(unchecked_document_id).document_id
   end
 end
