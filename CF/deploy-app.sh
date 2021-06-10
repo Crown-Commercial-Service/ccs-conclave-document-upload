cdbc387b39d6e66903f821a5a9c2e7f4e8423c71
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2021-06-10 09:00:12 +0100

    removed rolling deployment

--- CF/deploy-app.sh
+++ CF/deploy-app.sh
@@ -121,4 +121,4 @@ sed "s/CF_SPACE/$CF_SPACE/g" manifest.yml | sed "s/MEMORY_LIMIT/$MEMORY_LIMIT/g"
 # deploy
 cd .. || exit
 
-cf push ccs-conclave-document-upload -f CF/"$CF_SPACE".manifest.yml --strategy rolling
+cf push ccs-conclave-document-upload -f CF/"$CF_SPACE".manifest.yml
