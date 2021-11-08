e77dfe17e208844f3b72925dce9c5f951c151aae
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2021-11-08 16:06:30 +0000

    addressed rubocop issue

--- app/controllers/document_upload_controller.rb
+++ app/controllers/document_upload_controller.rb
@@ -4,17 +4,16 @@ class DocumentUploadController < ApplicationController
   rescue_from Authorize::ClientService::ApiError, with: :return_error_code
   before_action :validate_api_key
   before_action :validate_client
+  before_action :find_client
+  before_action :find_unchecked_document
 
   def create
-    @client = Client.find_by(api_key: request.headers['x-api-key'])
-    unchecked_document = @client.unchecked_documents.new(document_parameters.reject { |_, v| v.blank? })
-
-    if unchecked_document.save
-      CallCheckServiceWorker.perform_async(unchecked_document.id, @client.api_key, request.headers['Authorization'])
-      document = unchecked_document.document.as_json.deep_transform_keys! { |key| key.camelize(:lower) }
+    if @unchecked_document.save
+      CallCheckServiceWorker.perform_async(@unchecked_document.id, @client.api_key, request.headers['Authorization'])
+      document = camelize_keys(@unchecked_document.document)
       render json: document, status: :created
     else
-      render json: unchecked_document.errors, status: :unprocessable_entity
+      render json: @unchecked_document.errors, status: :unprocessable_entity
     end
   end
 
@@ -23,6 +22,18 @@ class DocumentUploadController < ApplicationController
   def document_parameters
     params.permit(:documentFile, :documentFilePath, :sourceApp, :sizeValidation,
                   typeValidation: []).transform_keys!(&:underscore)
+  end
+
+  def find_client
+    @client = Client.find_by(api_key: request.headers['x-api-key'])
+  end
+
+  def find_unchecked_document
+    @unchecked_document = @client.unchecked_documents.new(document_parameters.reject { |_, v| v.blank? })
+  end
+
+  def camelize_keys(document)
+    document.as_json.deep_transform_keys! { |key| key.camelize(:lower) }
   end
 
   def return_error_code(code)
