833bf02482d83bac3af6da9256d0e862320c1f66
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2022-05-05 14:44:44 +0100

    addressing rubocop issues

--- config/initializers/aws_ssm.rb
+++ config/initializers/aws_ssm.rb
@@ -3,27 +3,29 @@ require 'aws-sdk-ssm'
 private
 
 def config_aws
-	params_list = []
-	ssm_client = nil
-	vcap_services = JSON.parse(ENV['VCAP_SERVICES'])
+  params_list = []
+  ssm_client = nil
+  vcap_services = JSON.parse(ENV['VCAP_SERVICES'])
 
-	vcap_services['user-provided'].each do |user_service|
-		params_list = user_service['credentials']['VARS_LIST'] if user_service['credentials']['VARS_LIST'].present?
-		if user_service['credentials']['aws_access_key_id'].present?
-			ssm_client = Aws::SSM::Client.new(
-				region: user_service['credentials']['region'],
-				access_key_id: user_service['credentials']['aws_access_key_id'],
-				secret_access_key: user_service['credentials']['aws_secret_access_key']
-			)
-		end
-	end
-	set_env(ssm_client, params_list) if ssm_client && params_list.any?
+  vcap_services['user-provided'].each do |user_service|
+    params_list = user_service['credentials']['VARS_LIST'] if user_service['credentials']['VARS_LIST'].present?
+    next if user_service['credentials']['aws_access_key_id'].blank?
+
+    ssm_client = Aws::SSM::Client.new(
+      region: user_service['credentials']['region'],
+      access_key_id: user_service['credentials']['aws_access_key_id'],
+      secret_access_key: user_service['credentials']['aws_secret_access_key']
+    )
+  end
+  set_env(ssm_client, params_list) if ssm_client && params_list.any?
 end
 
 def set_env(ssm_client, params_list)
-	params_list.each do |param_name| # The below line should change
-		ENV[param_name] = ssm_client.get_parameter({ name: "/conclave-document-upload/#{param_name}", with_decryption: true })[:parameter][:value]
-	end
+  params_list.each do |param_name| # The below line should change
+    ENV[param_name] =
+      ssm_client.get_parameter({ name: "/conclave-document-upload/#{param_name}",
+                                 with_decryption: true })[:parameter][:value]
+  end
 end
 
 config_aws if ENV['SERVER_ENV_NAME'].present?
