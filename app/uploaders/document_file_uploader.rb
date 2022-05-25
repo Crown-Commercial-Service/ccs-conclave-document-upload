c42cebf0f334383936e8fb7436f2285cab3f5191
@tberey <tomberey1@gmail.com>
2022-05-25 15:01:19 +0100

    CON-2317-Allow-Further-File-Extensions

--- app/uploaders/document_file_uploader.rb
+++ app/uploaders/document_file_uploader.rb
@@ -23,6 +23,7 @@ class DocumentFileUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w[pdf ppt pptx csv xls xlsx doc docx odt odp ods odg zip rar tar.gz tgz kml jpg jpeg png bmp tiff tif eps rdf rtf txt xml]
+    %w[pdf ppt pptx csv xls xlsx doc docx odt odp ods odg zip rar tar.gz tgz kml jpg jpeg png bmp tiff tif eps rdf rtf
+       txt xml]
   end
 end
