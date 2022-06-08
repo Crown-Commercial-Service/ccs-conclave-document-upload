a123b99456ba7db2d0847db1abb184ab5c786e51
@tberey <tomberey1@gmail.com>
2022-06-08 16:07:43 +0100

    CON-758-Security-Service-With-AndOr-Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,22 +23,24 @@ class ApplicationController < ActionController::API
 
   def validate_client_id
     decoded_token = validate_and_decode_token
-    raise ActionController::BadRequest.new('Not authorized: Missing Client ID') if decoded_token[0]['aud'].blank?
+    raise ActionController::BadRequest, 'Not authorized: Missing Client ID' if decoded_token[0]['aud'].blank?
   end
 
   def validate_user_access_token
-    raise ActionController::BadRequest.new('Not authorized: Missing Access Token') if bearer_token(request.headers).blank?
+    raise ActionController::BadRequest, 'Not authorized: Missing Access Token' if bearer_token(request.headers).blank?
   end
 
   def validate_access_token
     decoded_token = validate_and_decode_token
-    validate_token = SecurityService::Auth.new(decoded_token[0]['aud'], bearer_token(request.headers)).sec_api_validate_token
-    raise ActionController::BadRequest.new('Not authorized: Invalid Access Token') if validate_token.blank?
+    validate_token = SecurityService::Auth.new(decoded_token[0]['aud'],
+                                               bearer_token(request.headers)).sec_api_validate_token
+    raise ActionController::BadRequest, 'Not authorized: Invalid Access Token' if validate_token.blank?
   end
 
   def validate_and_decode_token
     decoded_token = decode_token(request.headers)
-    raise ActionController::BadRequest.new('Not authorized: Missing Access Token') if decoded_token.blank?
+    raise ActionController::BadRequest, 'Not authorized: Missing Access Token' if decoded_token.blank?
+
     decoded_token
   end
 
