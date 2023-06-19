9bcbdf507142dfdc4a11618f2c2d4433354aa140
georgeschena <georgeschena@hotmail.com>
2023-06-19 10:34:35 +0100

    Add Healthcheck endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate
+    # Skip authentication for the health_check route
+    return if request.path == '/health_check'
+
     request.headers['Authorization'] = request.headers['x-api-key']
     authenticate_or_request_with_http_basic do |source_app, api_key|
       @client = Client.find_by(source_app: source_app)
