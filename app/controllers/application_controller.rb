2f45e5920e8fa81db332b46ef771189aaead9a51
SamB-CCS <sam.boothroyd@crowncommercial.gov.uk>
2023-08-31 16:44:02 +0100

    update controller to add JWT authorisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+require 'jwt'
+require 'net/http'
+
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
   include ActionController::HttpAuthentication::Token::ControllerMethods
@@ -6,10 +9,34 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate
-    request.headers['Authorization'] = request.headers['x-api-key']
-    authenticate_or_request_with_http_basic do |source_app, api_key|
-      @client = Client.find_by(source_app: source_app)
-      @client && @client.api_key == api_key
+    auth_header = request.headers['Authorization']
+    token = auth_header.split(' ').last if auth_header
+
+    if !token.nil?
+      begin
+        decoded_token = JWT.decode(token, nil, false)
+        aud = decoded_token[0]['aud']
+        uri = URI.parse('https://dev.ppg-sso-service.crowncommercial.gov.uk/security/tokens/validation?client-id=`')
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        request = Net::HTTP::Post.new(uri.path)
+        request.body = "aud=#{aud}"
+        response = http.request(request)
+        response.code == '200'
+
+      rescue JWT::DecodeError
+        render json: { error: 'Invalid token' }, status: :unauthorized
+      end
+    else
+      request.headers['Authorization'] = request.headers['x-api-key']
+      authenticate_or_request_with_http_basic do |source_app, api_key|
+        @client = Client.find_by(source_app: source_app)
+        @client && @client.api_key == api_key
+      end  
     end
   end
 end
+
+
